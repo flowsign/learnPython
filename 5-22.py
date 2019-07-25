06c7f3fb1a4566873753c8dbefdeb9deef3e3014
WenH <flowsign@icloud.com>
2019-07-25 11:44:32 +0800

    第二个
    在网上看到的简单的方法

--- 5-22.py
+++ 5-22.py
@@ -1,27 +1,17 @@
-#随机分配8个老师到3个办公室
-
 import random
+teachers=['11','22','33','44','55','66','77','88']
+random.shuffle(teachers)
+office1=teachers[:2]
+office2=teachers[2:4]
+office3=teachers[4:6]
+office=[office1,office2,office3]
 
-#定义老师和办公室
-teachers = ['aaa','bbb','ccc','ddd','eee','fff','ggg','hhh']
-offices = [[],[],[]]
-
-#每个办公室随机分两个
-for officeNo in offices:
-        while len(officeNo) < 2:
-                teacherSum = len(teachers) - 1
-                index1 = random.randint(0,teacherSum)
-                officeNo.append(teachers[index1])
-                del teachers[index1]
-
-#随机分配剩下两个
-for name in teachers:
-    index2 =random.randint(0,2)
-    offices[index2].append(name)
+random.choice(office).append(teachers[6])
+random.choice(office).append(teachers[7])
 
 #输出
 i = 1
-for room in offices:
+for room in office:
     print("办公室%d"%i)
     for teacherName in room:
         print(teacherName)
